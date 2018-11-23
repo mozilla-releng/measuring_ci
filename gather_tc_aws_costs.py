76ebd5a5ddfbceef78ad5391cc22f3f25e908caf
Justin Wood <Callek@gmail.com>
2018-11-23 11:45:47 -0500

    More skeleton stuff

--- gather_tc_aws_costs.py
+++ gather_tc_aws_costs.py
@@ -1,7 +1,9 @@
 import argparse
 import asyncio
+from datetime import datetime
 import logging
 import os
+import re
 
 import boto3
 import pandas as pd
@@ -21,60 +23,81 @@ def parse_args():
     return parser.parse_args()
 
 
-async def fetch_raw_cost_explorer(config):
+def iter_cost_and_usage_groups(config):
     ce = boto3.client(
         'ce',
         aws_access_key_id='<redacted>',
         aws_secret_access_key='<redacted>'
         )
-    """ groupBy=TagKeyValue:WorkerType
-        forecastTimeRangeOption=None
-        hasBlended=false
-        excludeRefund=false
-        excludeCredit=false
-        excludeRIUpfrontFees=false
-        excludeRIRecurringCharges=false
-        excludeOtherSubscriptionCosts=false
-        excludeSupportCharges=false
-        excludeTax=false
-        excludeTaggedResources=false
-        chartStyle=Stack
-        timeRangeOption=LastMonth
-        granularity=Monthly
-        reportName=Monthly%20EC2%20running%20hours%20costs%20and%20usage
-        isTemplate=true
-        filter=[
-            {
-                "values":
-                    [{"value":"EC2: Running Hours","unit":"Hrs"}],
-                "dimension":"UsageTypeGroup",
-                "include":true,
-                "children":null
-            }]
-        reportType=CostUsage
-        hasAmortized=false
-        startDate=2018-09-01&endDate=2018-09-30
-    """
-    response = ce.get_cost_and_usage(
-        TimePeriod={
-            'Start': '2018-09-01',
-            'End': '2018-10-01'
+    next_page_token = None
+    while True:  # break below
+        kwargs = dict(
+            TimePeriod={
+                'Start': '2018-09-01',  # Inclusive
+                'End': '2018-10-01',  # Exclusive
+                },
+            Granularity='MONTHLY',  # MONTHLY, DAILY, HOURLY
+            GroupBy=[{'Type': 'TAG', 'Key': 'WorkerType'}],
+            Metrics=['UnblendedCost', 'UsageQuantity'],
+            Filter={
+                'Dimensions': {
+                    'Key': 'USAGE_TYPE_GROUP',
+                    'Values': ["EC2: Running Hours"],
+                },
             },
-        Granularity='MONTHLY',
-        GroupBy=[{'Type': 'TAG', 'Key': 'WorkerType'}],
-        Metrics=['UnblendedCost', 'UsageQuantity'],
-        Filter={
-            'Dimensions': {
-                'Key': 'USAGE_TYPE_GROUP',
-                'Values': ["EC2: Running Hours"],
-            },
-        },
         )
-    #import pdb;pdb.set_trace()
-    #import pprint
-    #pprint.pprint(response)
-    import json
-    print(json.dumps(response))
+        if next_page_token:
+            kwargs['NextPageToken'] = next_page_token
+            next_page_token = None
+        response = ce.get_cost_and_usage(**kwargs)
+        log.debug(response['ResponseMetadata'])
+        next_page_token = response.get('NextPageToken')
+        # We don't expect multiple results by time per response
+        assert len(response['ResultsByTime']) == 1
+        yield from response['ResultsByTime'][0]['Groups']
+        if not next_page_token:
+            break
+
+
+def split_worker_tag(tag):
+    data = tag.split('/', maxsplit=1)
+    if not data:
+        raise ValueError("bad tag")
+    if len(data) > 1:
+        return data
+    return ["none", data[0]]
+
+
+async def fetch_raw_cost_explorer(config):
+    worker_type_re = re.compile(re.escape(r'WorkerType$'))
+    rows = []
+    for group in iter_cost_and_usage_groups(config):
+        # Expect only 1 key
+        assert len(group['Keys']) == 1
+        key = group['Keys'][0]
+        if not worker_type_re.match(group['Keys'][0]):
+            raise Exception("Unexpected Key in Group, {}".format(group))
+        key = worker_type_re.sub('', key)
+        if not key:
+            key = "<untagged>"
+        provisioner , worker_type = split_worker_tag(key)
+        row = {
+            'modified': datetime.now(),  # XXX Fixup
+            'year': '2018',
+            'month': '09',
+            'provider': 'aws',
+            'provisioner': provisioner,
+            'worker_type': worker_type,
+            'usage_hours': group['Metrics']['UsageQuantity']['Amount'],
+            'cost': group['Metrics']['UnblendedCost']['Amount'],
+        }
+        #import pdb;pdb.set_trace()
+        import pprint
+        pprint.pprint(row)
+        rows.append(row)
+        #import json
+        #print(json.dumps(response))
+    import pdb;pdb.set_trace()
 
 
 async def main(args):
