d8e1eeea907255eedfd5838aa657ee51e0c54fd1
Justin Wood <Callek@gmail.com>
2018-12-27 15:47:46 -0500

    Finish cost explorer fetching

--- gather_tc_aws_costs.py
+++ gather_tc_aws_costs.py
@@ -1,6 +1,6 @@
 import argparse
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta, date
 import logging
 import os
 import re
@@ -19,22 +19,31 @@ log.setLevel(LOG_LEVEL)
 def parse_args():
     """Extract arguments."""
     parser = argparse.ArgumentParser(description="CI Costs")
-    parser.add_argument('--config', type=str, default='aws_tc_costs.yml')
+    parser.add_argument('--config', type=str, default=argparse.SUPPRESS)
+    parser.add_argument('--month', type=int, default=None,
+                        help="The month of costs to gather, as an integer where 0=Jan (default=<Last Full Month>)")
+    parser.add_argument('--year', type=int, default=None,
+                        help="The month of costs to gather, as an integer where 0=Jan (default=<most recent year of specified month>)")
     return parser.parse_args()
 
 
 def iter_cost_and_usage_groups(config):
     ce = boto3.client(
         'ce',
-        aws_access_key_id='AKIAJNJITBKBSUO67ORQ',
-        aws_secret_access_key='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'
+        aws_access_key_id=config['TC_AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=config['TC_AWS_SECRET_ACCESS_KEY']
         )
     next_page_token = None
+    # Start Date -- Inclusive
+    start_date = datetime(month=config['month'], year=config['year'], day=1)
+    # End Date -- Exclusive
+    # End always on first of month, but timedelta by enough days to always jump a month...
+    end_date = (start_date + timedelta(days=32)).replace(day=1)
     while True:  # break below
         kwargs = dict(
             TimePeriod={
-                'Start': '2018-09-01',  # Inclusive
-                'End': '2018-10-01',  # Exclusive
+                'Start': start_date.strftime("%Y-%m-%d"),
+                'End': end_date.strftime("%Y-%m-%d"),
                 },
             Granularity='MONTHLY',  # MONTHLY, DAILY, HOURLY
             GroupBy=[{'Type': 'TAG', 'Key': 'WorkerType'}],
@@ -49,6 +58,7 @@ def iter_cost_and_usage_groups(config):
         if next_page_token:
             kwargs['NextPageToken'] = next_page_token
             next_page_token = None
+        log.info("Getting a page of cost data from TC aws...")
         response = ce.get_cost_and_usage(**kwargs)
         log.debug(response['ResponseMetadata'])
         next_page_token = response.get('NextPageToken')
@@ -84,8 +94,8 @@ async def fetch_raw_cost_explorer(config):
         provisioner, worker_type = split_worker_tag(key)
         row = {
             'modified': modified_str,  # XXX Fixup
-            'year': 2018,
-            'month': 9,
+            'year': config['year'],
+            'month': config['month'],
             'provider': 'aws',
             'provisioner': provisioner,
             'worker_type': worker_type,
@@ -101,32 +111,90 @@ async def fetch_raw_cost_explorer(config):
 
 async def update_worker_costs(config):
     df_ce = await fetch_raw_cost_explorer(config)
-    #import pdb;pdb.set_trace()
-    #df = pd.DataFrame(rows,
-    #                  columns=['modified', 'year', 'month', 'provider', 'provisioner',
-    #                           'worker_type', 'usage_hours', 'cost'])
-    df = pd.read_csv('/home/callek/mozilla/obj/measure_ci/aws_cost_estimates.csv')
-    #df = df.append(rows, ignore_index=True)
-    import pdb;pdb.set_trace()
-    # ??
-    df[(df['month'].isin(df_ce['month'].unique()) & df['year'].isin(df_ce['year'].unique()))]
+    log.info("Reading cost data from existing: {}".format(config['costs_csv_file']))
+    df = pd.read_csv(config['costs_csv_file'])
 
+    new_records = []
+    for row in df_ce.itertuples(index=False):
+        def boolean_mask_existing_row(df):
+            worker_type_mask = (df['worker_type'] == row.worker_type)
+            if row.worker_type == "<untagged>":
+                # support old imported csv goop
+                worker_type_mask = (
+                    worker_type_mask |
+                    (df['worker_type'] == 'No Tagkey: WorkerType')
+                )
+            return (
+                (df['month'] == row.month) &
+                (df['year'] == row.year) &
+                (df['provider'] == row.provider) &
+                (df['provisioner'] == row.provisioner) &
+                (worker_type_mask)
+                )
+
+        # Set these again, warn if different
+        overlapped_row_costs = df.loc[
+            boolean_mask_existing_row,
+            ('worker_type', 'usage_hours', 'cost')
+            ]
+        if overlapped_row_costs.empty:
+            new_records.append(row)
+        else:
+            update_tuple = (row.worker_type, row.usage_hours, row.cost)
+            if tuple(overlapped_row_costs.values[0]) == update_tuple:
+                # This test is a bit fragile, as it doesn't account for rounding differences such as float precision.
+                continue
+            # Not Empty, update for differences
+            # use log.debug to avoid false positives in the warning.
+            log.debug("Data differs and entry already exists for row `{}`, "
+                      "updating existing `{}`".format(row, overlapped_row_costs.to_dict(orient='records')[0])
+                      )
+            df.loc[
+                boolean_mask_existing_row,
+                ('worker_type', 'usage_hours', 'cost')
+            ] = (row.worker_type, row.usage_hours, row.cost)
+
+    new_df_records = pd.DataFrame.from_records(new_records,
+                                               columns=['modified', 'year', 'month', 'provider', 'provisioner',
+                                                        'worker_type', 'usage_hours', 'cost'])
+    df = df.append(new_df_records)
+    return df
 
 
 async def main(args):
     """Main program."""
     with open(args['config'], 'r') as cfg:
         config = yaml.load(cfg)
-    os.environ['TC_CACHE_DIR'] = config['TC_CACHE_DIR']
+    today = date.today()
+    if args['month']:
+        config['month'] = args['month']
+    else:
+        config['month'] = (today.replace(day=1) - timedelta(days=1)).month
+    if args['year']:
+        config['year'] = args['year']
+    else:
+        if today.replace(month=config['month'], day=1) >= today.replace(day=1):
+            config['year'] = (today.replace(month=1, day=1) - timedelta(days=1)).year
+        else:
+            config['year'] = today.year
 
-    await update_worker_costs(config)
+    # Enforce access keys for TC AWS
+    if 'TC_AWS_ACCESS_KEY_ID' in os.environ and 'TC_AWS_SECRET_ACCESS_KEY' in os.environ:
+        config['TC_AWS_ACCESS_KEY_ID'] = os.environ['TC_AWS_ACCESS_KEY_ID']
+        config['TC_AWS_SECRET_ACCESS_KEY'] = os.environ['TC_AWS_SECRET_ACCESS_KEY']
+    else:
+        raise Exception("TC_AWS_ACCESS_KEY_ID and TC_AWS_SECRET_ACCESS_KEY are required to be passed in environment")
+
+    new_df = await update_worker_costs(config)
+    log.info("Writing updated file to {}".format(config['costs_csv_file']))
+    new_df.to_csv(config['costs_csv_file'], index=False)
 
 
 def lambda_handler(args, context):
     """AWS Lambda entry point."""
     assert context  # not currently used
     if 'config' not in args:
-        args['config'] = 'nightlies.yml'
+        args['config'] = 'update_costs.yml'
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(args))
 
